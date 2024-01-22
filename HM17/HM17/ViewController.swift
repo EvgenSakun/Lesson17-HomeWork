9b3b0c03f97316d9635fb4d4f7ca52cc8edf6a4a
EvgenSakun <evgen.sakun20@gmail.com>
2024-01-22 17:28:20 +0300

    start

---
+++ HM17/HM17/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HM17
+//
+//  Created by Evgeny Sakun on 22.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    
+
+}
+
